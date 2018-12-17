6e69ba8746865eec902499541baed0d47d83b0a2
David Moles <david.moles@ucop.edu>
2018-12-17 14:23:56 -0800

    Improve error handling

--- cmd/fixity.go
+++ cmd/fixity.go
@@ -6,44 +6,52 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"log"
 	"net/url"
 	"strings"
 )
 
-const longDescription = `
-Verify the digest of a file
+const shortDescription = "Verify the digest of an object"
+
+const longDescription = shortDescription + `
 [TODO: long description]
+`
+
+const example = `
+  fixity s3://mybucket/myprefix/myobject
+  fixity s3://mybucket/myprefix/myobject -e 9f1d6f7b77f74a091c7734ae15f394be3f67c73a8568d623197c01ba4f05e9ff
+  fixity s3://mybucket/myprefix/myobject -a md5 f7f647f7d9338fb16ba3a1da8453fee3
 `
 
 var expectedDigest *[]byte
 var digestAlgorithm *string
 
 var fixityCmd = &cobra.Command{
-	Use: "fixity <URL>",
-	Short: "Verify the digest of an object",
+	Use: "fixity <OBJECT-URL>",
+	Short: shortDescription,
 	Long: strings.TrimSpace(longDescription),
 	Args: cobra.ExactArgs(1),
-	Example: `
-      fixity s3://mybucket/myprefix/myobject
-      fixity s3://mybucket/myprefix/myobject -e 9f1d6f7b77f74a091c7734ae15f394be3f67c73a8568d623197c01ba4f05e9ff
-      fixity s3://mybucket/myprefix/myobject -a md5 f7f647f7d9338fb16ba3a1da8453fee3
-    `,
-	Run: func(cmd *cobra.Command, args []string) {
-		checkFixity(args[0])
+	SilenceUsage: true,
+	SilenceErrors: true,
+	Example: "  " + strings.TrimSpace(example),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return checkFixity(args[0])
 	},
 }
 
-func checkFixity(objUrlStr string) {
-	fmt.Printf("expectedDigest  : %x\n", *expectedDigest)
-	fmt.Printf("digestAlgorithm : %s\n", *digestAlgorithm)
+func checkFixity(objUrlStr string) error {
 	objUrl, err := url.Parse(objUrlStr)
-	if err != nil {
-		log.Fatal(err) // TODO: figure out why url.Parse never fails
+	if err == nil {
+		return checkFixityUrl(objUrl)
 	}
-	fmt.Println("Object URL: " + objUrl.String())
+	return err
 }
 
+func checkFixityUrl(objUrl *url.URL) error {
+	fmt.Printf("Object URL: %v\n", objUrl)
+	fmt.Printf("expectedDigest  : %x\n", *expectedDigest)
+	fmt.Printf("digestAlgorithm : %s\n", *digestAlgorithm)
+	return nil
+}
 
 func init() {
 	// TODO: add download option
