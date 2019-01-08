2366e62085efbd29faf2b8cc002f469b597ae544
David Moles <david.moles@ucop.edu>
2019-01-07 16:40:49 -0800

    Extract AWS utility methods, clean up code

--- cos/check.go
+++ cos/check.go
@@ -10,23 +10,12 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
-	"reflect"
-	"regexp"
-	"unsafe"
-
-	"github.com/aws/aws-sdk-go/aws/credentials"
 
 	. "github.com/dmolesUC3/cos/util"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
-
-const DefaultAwsRegion = "us-west-2"
-
-var awsRegionRegexp = regexp.MustCompile("https?://s3-([^.]+)\\.amazonaws\\.com/")
 
 type Check struct {
 	Logger    Logger
@@ -36,31 +25,31 @@ type Check struct {
 	Region    string
 }
 
-func (f Check) GetDigest() ([]byte, error) {
-	sess, err := f.initSession()
+func (c Check) GetDigest() ([]byte, error) {
+	c.Logger.Detail("Initializing session")
+	sess, err := InitSession(c.endpointP(), c.regionStrP(), c.Logger.Verbose())
 	if err != nil {
 		return nil, err
 	}
 
 	// TODO: don't write to tempfile
-	outfile, err := ioutil.TempFile("", f.objFilename())
+	outfile, err := ioutil.TempFile("", c.objFilename())
 	if err != nil {
 		return nil, err
 	}
 	defer func() {
 		err := os.Remove(outfile.Name())
 		if err != nil {
-			f.Logger.Info(err)
+			c.Logger.Info(err)
 		}
 	}()
-	f.Logger.Detailf("Downloading to tempfile: %v\n", outfile.Name())
-
+	c.Logger.Detailf("Downloading to tempfile: %v\n", outfile.Name())
 	downloader := s3manager.NewDownloader(sess)
 	bytesDownloaded, err := downloader.Download(outfile, &s3.GetObjectInput{
-		Bucket: f.bucketP(),
-		Key:    f.keyP(),
+		Bucket: c.bucketP(),
+		Key:    c.keyP(),
 	})
-	f.Logger.Detailf("Downloaded %d bytes\n", bytesDownloaded)
+	c.Logger.Detailf("Downloaded %d bytes\n", bytesDownloaded)
 	if err != nil {
 		return nil, err
 	}
@@ -73,106 +62,59 @@ func (f Check) GetDigest() ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	h := f.newHash()
+	h := c.newHash()
 	bytesHashed, err := io.Copy(h, infile)
-	f.Logger.Detailf("Hashed %d bytes\n", bytesHashed)
+	c.Logger.Detailf("Hashed %d bytes\n", bytesHashed)
 	if err != nil {
 		return nil, err
 	}
-
 	digest := h.Sum(nil)
 
-	if len(f.Expected) > 0 {
-		if !bytes.Equal(f.Expected, digest) {
-			err = fmt.Errorf("digest mismatch: expected: %x, actual: %x", f.Expected, digest)
+	if len(c.Expected) > 0 {
+		if !bytes.Equal(c.Expected, digest) {
+			err = fmt.Errorf("digest mismatch: expected: %x, actual: %x", c.Expected, digest)
 		}
 	}
 
 	return digest, err
 }
 
-func (f Check) newHash() hash.Hash {
-	if f.Algorithm == "sha256" {
+func (c Check) newHash() hash.Hash {
+	if c.Algorithm == "sha256" {
 		return sha256.New()
 	}
 	return md5.New()
 }
 
-func (f Check) initSession() (*session.Session, error) {
-	f.Logger.Detail("Initializing session")
-	endpointP := f.endpointP()
-	s3Config := aws.Config{
-		Endpoint:         endpointP,
-		Region:           f.regionStrP(),
-		S3ForcePathStyle: aws.Bool(true),
-		CredentialsChainVerboseErrors: aws.Bool(f.Logger.Verbose()),
+func (c Check) regionStrP() *string {
+	if c.Region != "" {
+		c.Logger.Detailf("Using specified AWS region: %v\n", c.Region)
+		return &c.Region
 	}
-	s3Opts := session.Options{
-		Config:            s3Config,
-		SharedConfigState: session.SharedConfigEnable,
-	}
-	sess, err := session.NewSessionWithOptions(s3Opts)
-	if err != nil {
-		return nil, err
-	}
-	return validateCredentials(sess)
-}
-
-// TODO: https://github.com/aws/aws-sdk-go/issues/2392
-func validateCredentials(sess *session.Session) (*session.Session, error) {
-	providerVal := reflect.ValueOf(*sess.Config.Credentials).FieldByName("provider").Elem()
-	if providerVal.Type() == reflect.TypeOf((*credentials.ChainProvider)(nil)) {
-		chainProvider := (*credentials.ChainProvider)(unsafe.Pointer(providerVal.Pointer()))
-		providers := chainProvider.Providers
-		if len(providers) > 0 {
-			err := reflect.ValueOf(providers[0]).Elem().FieldByName("Err")
-			if err.IsValid() {
-				if e2, ok := err.Interface().(error); ok {
-					return nil, e2
-				}
-			}
-		}
-	}
-	return sess, nil
-}
-
-func (f Check) regionStrP() *string {
-	if f.Region != "" {
-		f.Logger.Detailf("Using specified AWS region: %v\n", f.Region)
-		return &f.Region
-	}
-	endpoint := f.endpointStr()
-	matches := awsRegionRegexp.FindStringSubmatch(endpoint)
-	regionStr := DefaultAwsRegion
-	if len(matches) == 2 {
-		f.Logger.Detailf("Found AWS region in endpoint URL %v: %v\n", endpoint, regionStr)
-		regionStr = matches[1]
-	} else {
-		f.Logger.Detailf("No AWS region found in endpoint URL %v; using default region %v\n", endpoint, DefaultAwsRegion)
-	}
+	endpoint := c.endpointStr()
+	regionStr := ExtractRegion(endpoint, c.Logger)
 	return &regionStr
 }
 
-func (f Check) endpointStr() string {
-	return f.ObjLoc.Endpoint.String()
+func (c Check) endpointStr() string {
+	return c.ObjLoc.Endpoint.String()
 }
 
-func (f Check) endpointP() *string {
-	endpointUrlStr := f.endpointStr()
+func (c Check) endpointP() *string {
+	endpointUrlStr := c.endpointStr()
 	return &endpointUrlStr
 }
 
-func (f Check) objFilename() string {
-	return path.Base(f.ObjLoc.Key())
+func (c Check) objFilename() string {
+	return path.Base(c.ObjLoc.Key())
 }
 
-func (f Check) bucketP() *string {
-	bucket := f.ObjLoc.Bucket()
+func (c Check) bucketP() *string {
+	bucket := c.ObjLoc.Bucket()
 	return &bucket
 }
 
-func (f Check) keyP() *string {
-	key := f.ObjLoc.Key()
+func (c Check) keyP() *string {
+	key := c.ObjLoc.Key()
 	return &key
 }
---
+++ util/aws_utils.go
@@ -0,0 +1,64 @@
+package util
+
+import (
+	"reflect"
+	"regexp"
+	"unsafe"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+)
+
+const DefaultAwsRegion = "us-west-2"
+var awsRegionRegexp = regexp.MustCompile("https?://s3-([^.]+)\\.amazonaws\\.com/")
+
+func ExtractRegion(endpoint string, logger Logger) string {
+	matches := awsRegionRegexp.FindStringSubmatch(endpoint)
+	regionStr := DefaultAwsRegion
+	if len(matches) == 2 {
+		regionStr = matches[1]
+		logger.Detailf("Found AWS region in endpoint URL %v: %v\n", endpoint, regionStr)
+	} else {
+		logger.Detailf("No AWS region found in endpoint URL %v; using default region %v\n", endpoint, DefaultAwsRegion)
+	}
+	return regionStr
+}
+
+func InitSession(endpointP *string, regionStrP *string, verbose bool) (*session.Session, error) {
+	s3Config := aws.Config{
+		Endpoint:                      endpointP,
+		Region:                        regionStrP,
+		S3ForcePathStyle:              aws.Bool(true),
+		CredentialsChainVerboseErrors: aws.Bool(verbose),
+	}
+	s3Opts := session.Options{
+		Config:            s3Config,
+		SharedConfigState: session.SharedConfigEnable,
+	}
+	sess, err := session.NewSessionWithOptions(s3Opts)
+	if err != nil {
+		return nil, err
+	}
+	return validateCredentials(sess)
+}
+
+// TODO: https://github.com/aws/aws-sdk-go/issues/2392
+func validateCredentials(sess *session.Session) (*session.Session, error) {
+	providerVal := reflect.ValueOf(*sess.Config.Credentials).FieldByName("provider").Elem()
+	if providerVal.Type() == reflect.TypeOf((*credentials.ChainProvider)(nil)) {
+		chainProvider := (*credentials.ChainProvider)(unsafe.Pointer(providerVal.Pointer()))
+		providers := chainProvider.Providers
+		if len(providers) > 0 {
+			err := reflect.ValueOf(providers[0]).Elem().FieldByName("Err")
+			if err.IsValid() {
+				if e2, ok := err.Interface().(error); ok {
+					return nil, e2
+				}
+			}
+		}
+	}
+	return sess, nil
+}
+
+
